5a4ea1039fb67b8be3510dee30ff552f8bd883d8
Tommy H. Yeargin, Jr <yeargin2018@gmail.com>
2018-09-16 21:17:06 -0400

    Firs commit to Deadpan_Fireman repository.

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(300, 500);
+}
+
+function draw() {
+  background(220, 0, 200);
+	/*rotate(45);*/
+  rect(30,30,239,190);
+  /*point(150,150);*/
+  line(30,300,265,300);
+  
+}
